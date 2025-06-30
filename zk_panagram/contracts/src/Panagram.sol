a2c75cc1206cae5eb9444087340a3af91f5a61e7
AshiqAhamed17 <aashiq121205@gmail.com>
2025-06-30 20:01:55 +0530

    Update Panagram.sol

--- zk_panagram/contracts/src/Panagram.sol
+++ zk_panagram/contracts/src/Panagram.sol
@@ -9,25 +9,31 @@ import {IVerifier} from "./Verifier.sol";
 contract Panagram is ERC1155, Ownable {
 
     ////////// State Variables ///////////////
-    IVerifier public  verifier;
+    IVerifier public s_verifier;
     bytes32 public s_answer;
     uint256 public constant MIN_DURATION = 10800; //3 hrs
     uint256 public s_roundStartTime;
     address public s_currentRoundWinner;
     uint256 public s_currentRound;
+    mapping (address => uint256) public s_lastCorrectGuessRound;
 
 
     //////////// EVENTS ///////////////
     event Panagram_VerifierUpdate(IVerifier verifier);
     event Panagram_NewRoundStarted(bytes32 answer);
+    event Panagram_WinnerCrowned(address indexed winner, uint256 round);
+    event Panagram_RunnerUpCrowned(address indexed runnerup, uint256 indexed round);
 
 
     //////////// ERROR //////////////
     error Panagram_MinTimeNotPassed(uint256 minTime, uint256 passedTime);
     error Panagram_NoRoundWinner();
+    error Panagram_FirstPanagramNotStarted();
+    error Panagram_AlreadyGuessedCorrectly(uint256 round, address guesser);
+    error Panagram_InvalidProof();
     
     constructor(IVerifier _verifier) ERC1155("ipfs://bafybeicqfc4ipkle34tgqv3gh7gccwhmr22qdg7p6k6oxon255mnwb6csi/{id}.json") Ownable(msg.sender){
-        verifier = _verifier;
+        s_verifier = _verifier;
     }
 
     ///function to create a new round
@@ -55,10 +61,42 @@ contract Panagram is ERC1155, Ownable {
     }
 
     /// Function to allow users to submit a guess
+    function makeGuess(bytes memory proof) external returns (bool) {
+        //Check weather the first round has been started
+        if(s_currentRound == 0) {
+            revert Panagram_FirstPanagramNotStarted();
+        }
+        //check if the user has already guessed correctly
+        if(s_lastCorrectGuessRound[msg.sender] == s_currentRound) {
+            revert Panagram_AlreadyGuessedCorrectly(s_currentRound, msg.sender);
+        }
+        //check the proof and verify it with the verifier contract
+        bytes32[] memory publicInputs = new bytes32[](1);
+        publicInputs[0] = s_answer;
+        bool proofResult = s_verifier.verify(proof, publicInputs);
+        if(!proofResult) {
+            revert Panagram_InvalidProof();
+        }
+        s_lastCorrectGuessRound[msg.sender] = s_currentRound;
+
+        //if correct check if they are first and mint them NFT with id 0
+        //if correct and not first then mint them NFT with id 1
+        if(s_currentRoundWinner == address(0)) {
+            s_currentRoundWinner = msg.sender;
+            _mint(msg.sender, 0, 1, "");
+            emit Panagram_WinnerCrowned(msg.sender, s_currentRound);
+        }
+        else {
+            _mint(msg.sender, 1, 1, "");
+            emit Panagram_RunnerUpCrowned(msg.sender, s_currentRound);
+        }
+
+        return proofResult;
+    }
 
     /// Sets a new Verifier
     function setVerifier(IVerifier _verifier) external onlyOwner {
-        verifier = _verifier;
+        s_verifier = _verifier;
         emit Panagram_VerifierUpdate(_verifier);
     }
 
