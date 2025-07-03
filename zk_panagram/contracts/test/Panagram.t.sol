a5597f8a2fde695f0b7cabbc9de3617cf8935a85
AshiqAhamed17 <aashiq121205@gmail.com>
2025-07-03 19:25:37 +0530

    updates

--- zk_panagram/contracts/test/Panagram.t.sol
+++ zk_panagram/contracts/test/Panagram.t.sol
@@ -10,7 +10,7 @@ contract PanagramTest is Test {
     HonkVerifier public verifier;
     uint256 constant MODULUS = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
     bytes32 ANSWER = bytes32(uint256(keccak256("triangles")) % MODULUS);
-
+    address user = makeAddr('user');
     function setUp() public {
         verifier = new HonkVerifier();
         panagram = new Panagram(verifier);
@@ -18,5 +18,27 @@ contract PanagramTest is Test {
         panagram.newRound(ANSWER);
     }
 
-    function testA() public {}
+    
+    function _getProof(bytes32 guess, bytes32 correctAnswer) internal returns (bytes memory _proof) {
+        uint256 NUM_ARGS = 5;
+        string [] memory inputs = new string [](NUM_ARGS);
+        inputs[0] = "npx";
+        inputs[1] = "tsx";
+        inputs[2] = "js-scripts/generateProof.ts";
+        inputs[3] = vm.toString(guess);
+        inputs[4] = vm.toString(correctAnswer);
+
+        bytes memory encodedProof = vm.ffi(inputs);
+        _proof = abi.decode(encodedProof, (bytes));
+
+        console.logBytes(_proof);
+
+    }
+
+    function testCorrectGuessPasses() public {
+        vm.prank(user);
+        bytes memory proof = _getProof(ANSWER, ANSWER);
+        panagram.makeGuess(proof);
+
+    }
 }
